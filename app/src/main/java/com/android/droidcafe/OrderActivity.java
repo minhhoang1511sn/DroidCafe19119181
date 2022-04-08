93d9da11b7067098761580453aaf5c376f39b794
minhhoang1511sn <80461876+minhhoang1511sn@users.noreply.github.com>
2022-04-08 14:58:15 +0700

    done

--- app/src/main/java/com/android/droidcafe/OrderActivity.java
+++ app/src/main/java/com/android/droidcafe/OrderActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
-public class OrderActivity extends AppCompatActivity {
+import android.widget.AdapterView;
+public class OrderActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,17 @@ public class OrderActivity extends AppCompatActivity {
                 intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textview);
         textView.setText(message);
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
     }
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message,
@@ -48,5 +63,16 @@ public class OrderActivity extends AppCompatActivity {
                 // Do nothing.
                 break;
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String spinnerLabel = adapterView.getItemAtPosition(i).toString();
+        displayToast(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
